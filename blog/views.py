39258c8b1b47a1c3c91439ce37bc62d6602571a8
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-07 15:27:08 +0100

    add bootstrap to the project.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # the index page
 def index(request):
-    return HttpResponse("<h1> Home page </h1>")
+    return render(request, 'assets/index.html')
