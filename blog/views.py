47e950b50777ffdf749bf2aa6e061fc1ddb9aec5
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-07 16:45:53 +0100

    fix some bugs.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from blog.models import Posts
-import random
 
 
 # the index page
@@ -10,11 +9,3 @@ def index(request):
     }
     return render(request, 'assets/index.html', context)
 
-
-def side_bar(request):
-    post_ids = Posts.objects.values_list('id', flat=True)
-    random_id = random.choice(post_ids)
-    context = {
-        "random_post": Posts.objects.get(pk=random_id)
-    }
-    return render(request, 'assets/side.html', context)
