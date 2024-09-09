75925166c8851595f659f98b1b5a69bab211e6ae
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-09 20:56:56 +0100

    finish search feature.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import index, post_detail
+from blog.views import index, post_detail, search_view
 from djogo import settings
 from django.conf.urls.static import static
 
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name='home'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('search/', search_view, name='search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from blog.models import Posts
-
+from django.db.models import Q
 
 # the index page
 def index(request):
@@ -14,3 +14,18 @@ def post_detail(request, pk):
     post = get_object_or_404(Posts, pk=pk)
     return render(request, 'assets/post_detail.html', {'post': post})
 
+
+def search_view(request):
+    query = request.GET.get('q')
+    results = []
+    if query:
+        results = Posts.objects.filter(
+            Q(title__icontains=query) | Q(content__icontains=query)
+        )
+
+    context = {
+        'results': results,
+        'query': query,
+    }
+
+    return render(request, 'assets/search_results.html', context)
