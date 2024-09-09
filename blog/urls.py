ca3d10f67facaefdbd67e78e84d5dedfd4500566
MLankaoui <marouanelankaoui66@gmail.com>
2024-09-09 15:02:34 +0100

    view post more details.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, post_detail
 from djogo import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', index, name='home'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from blog.models import Posts
 
 
@@ -8,4 +8,9 @@ def index(request):
         "post": Posts.objects.all()
     }
     return render(request, 'assets/index.html', context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Posts, pk=pk)
+    return render(request, 'assets/post_detail.html', {'post': post})
 
